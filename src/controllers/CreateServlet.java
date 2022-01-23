2ed6cd0df51e862f5d45a4957c82f6bbb513803b
takuma.mizushima <takuma.mizushima@axas-japan.co.jp>
2022-01-23 17:24:40 +0900

    validator実装

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.tasks;
+import models.validators.tasksValidator;
 import utils.DBUtil;
 
 @WebServlet("/create")
@@ -37,14 +40,30 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
-            /* 必要な情報をセットしたMessageクラスのオブジェクトをpersistメソッドを使ってデータベースにセーブする*/
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
+            m.setUpdated_at(currentTime);
 
-            // indexページへリダイレクト(遷移)させる
-            response.sendRedirect(request.getContextPath() + "/index");
+            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors = tasksValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("tasks", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースに保存
+                em.persist(m);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                // indexのページにリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 
